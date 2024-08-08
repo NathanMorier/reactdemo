3dc9103660a44b824211f31467ebff4b86ac3008
NathanSifuGaming <nathansifugaming@protonmail.com>
2024-08-08 13:21:12 -0300

    useeffect dependencies lesson complete

--- README.md
+++ README.md
@@ -463,3 +463,49 @@ useEffect(() => {
 ...
 
 There'll be more in the next lesson.
+
+
+!!!!! useEffect Dependencies !!!!! (useeffect-dependancies branch [sic])
+
+A bit of a continuation from last lesson:
+
+//Home.js
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
+
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: 'Its a-meee!', body: 'lorem ipsum...', author: 'mario', id: 1},
+    { title: 'Yahoooo!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
+    { title: 'Yehee, go green!', body: 'lorem ipsum...', author: 'luigi', id: 3}
+  ]);
+
+  const [name, setName] = useState('mario'); // add this
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
+  useEffect(() => {
+    console.log('use effect ran');
+    console.log(name);
+  }, [name]); // adding the name as a Dependencies DOES allow the useEffect to
+  // run again, same with any other added. Notice if you click on the button
+  // again, it DOESN'T run again, because it knows nothing has changed, it's
+  // still luigi
+  /*
+  }, []); adding an empty array is if you on makes sure the useEffect will
+  Only run once on the first render.
+  */
+
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      <button onClick={() => setName('luigi')}>change name</button>
+      <p>{ name }</p>
+    </div>
+  );
+}
+
+export default Home;
--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,17 +1,16 @@
 //Home.js
-import { useState, useEffect } from 'react'; // import useEffect from react
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([ // 'blogs' is essentially the name of the array
-    // Notice that 'setBlogs' is NO LONGER commented out, that's because it's
-    // actually being used.
-    { title: 'Its a-meee!', body: 'lorem ipsum...', author: 'mario', id: 1}, // id's MUST be unique
+  const [blogs, setBlogs] = useState([
+    { title: 'Its a-meee!', body: 'lorem ipsum...', author: 'mario', id: 1},
     { title: 'Yahoooo!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
     { title: 'Yehee, go green!', body: 'lorem ipsum...', author: 'luigi', id: 3}
   ]);
 
-  // New function to handleDelete
+  const [name, setName] = useState('mario'); // add this
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
@@ -19,12 +18,21 @@ const Home = () => {
 
   useEffect(() => {
     console.log('use effect ran');
-    console.log(blogs);
-  });
+    console.log(name);
+  }, [name]); // adding the name as a Dependencies DOES allow the useEffect to
+  // run again, same with any other added. Notice if you click on the button
+  // again, it DOESN'T run again, because it knows nothing has changed, it's
+  // still luigi
+  /*
+  }, []); adding an empty array is if you on makes sure the useEffect will
+  Only run once on the first render.
+  */
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} /> {/*new handleDelete prop*/}
+      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      <button onClick={() => setName('luigi')}>change name</button>
+      <p>{ name }</p>
     </div>
   );
 }
